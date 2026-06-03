24049e38f104d4956590f21b3efe910317f8e573
asc-chauhan <anshulasc151976@gmail.com>
2026-06-03 17:56:15 +0530

    fix: add certifi for MongoDB Atlas SSL connection

--- app/db/client.py
+++ app/db/client.py
@@ -1,6 +1,7 @@
 import os
+import certifi
 from pymongo import AsyncMongoClient
 
 MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb://admin:admin@mongodb:27017")
 
-mongo_client: AsyncMongoClient = AsyncMongoClient(MONGODB_URI)
+mongo_client: AsyncMongoClient = AsyncMongoClient(MONGODB_URI, tlsCAFile=certifi.where())
